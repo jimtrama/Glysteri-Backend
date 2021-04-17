607640ae81396b35a82b660350f18eb4a17e8050
jim trama <36377889+jimtrama@users.noreply.github.com>
2021-04-17 05:06:30 +0300

    added http req for ratings

--- app/routes/waiter.js
+++ app/routes/waiter.js
@@ -1,29 +1,14 @@
 const { MongoClient } = require("mongodb");
-const mac = require('macfromip');
-
+const mac = require("macfromip");
 
 function main(io) {
-
-    const callWaiter = function ({sunbed,id}) {
-        console.log(sunbed,id);
-        io.of('/admin').emit('waiter:called',{sunbed,id})
-    };
-    return {
-        callWaiter
-    }
-    // app.get('/call', (req, res) => {
-    //     let ip = req.connection.remoteAddress.split(":");
-    //     ip = ip[ip.length - 1];
-    //     console.log(ip);
-    //     mac.getMac(ip, (err, data) => {
-    //         console.log(data);
-    //         res.send(data)
-    //     });
-
-    // })
-   
-
+  const callWaiter = function ({ sunbed, id }) {
+    console.log(sunbed, id);
+    io.of("/admin").emit("waiter:called", { sunbed, id });
+  };
+  return {
+    callWaiter,
+  };
 }
 
 module.exports = main;
-
